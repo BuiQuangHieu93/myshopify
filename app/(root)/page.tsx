22f8942c0a815e09aa9db782f52ff207d4c9def1
BuiQuangHieu93 <zzzqhieu@gmail.com>
2024-03-03 19:10:10 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/shared/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        Home
+      </section>
+    </>
+  );
+}
